46e62161e9222a4146ddb5230966b4629c0acd9b
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-07-12 19:15:30 +0300

    Update files.py

--- files.py
+++ files.py
@@ -1,3 +1,3 @@
 from filesplit.split import Split
-split = Split("phenom_reportview_all.csv", "C:/Users/micha/Documents")
+split = Split("YOUR_FILE.csv", "C:/Users/user/Documents")
 split.bysize(size = 1024*1000*40, newline=True) # 40MB
